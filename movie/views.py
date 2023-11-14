5ab08ace0d724dfbb14b9cd7fb558c1f03aee5ab
Gökhan Bıyıkoğlu <gokhan_byk@icloud.com>
2023-11-14 21:34:14 +0300

    navbar eklendi

--- movie/views.py
+++ movie/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from user.models import *
 
 # Create your views here.
 def index_view(request):
@@ -7,4 +8,9 @@ def index_view(request):
     return render(request, 'movie/index.html', {})
 
 def movies_view(request, profile_slug):
-    pass
+    
+    profile = Profile.objects.get(slug = profile_slug)
+
+    return render(request, 'movie/movies.html', {
+        'profile': profile
+    })
