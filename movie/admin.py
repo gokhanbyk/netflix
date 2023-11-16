7577aa6bb209ff6af84464887856b37362d133f2
Gökhan Bıyıkoğlu <gokhan_byk@icloud.com>
2023-11-16 21:16:10 +0300

    movie modeli oluşturuldu

--- movie/admin.py
+++ movie/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import *
+from django.utils.safestring import mark_safe
 
 # Register your models here.
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'show_genre')
+
+    def show_genre(self, obj):
+        liste = '<ul>'
+        for i in obj.genre.all():
+            liste += f'<li>{i}</li>'
+        liste += '</ul>'
+        return mark_safe(liste)
+
+
+
+admin.site.register(Category)
+admin.site.register(Genre)
+admin.site.register(Movies, MovieAdmin)
--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from autoslug import AutoSlugField
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+    slug = AutoSlugField(populate_from = 'name')
+
+    def __str__(self):
+        return self.name
+
+class Genre(models.Model):
+    name = models.CharField(max_length=30)
+    slug = AutoSlugField(populate_from = 'name')
+
+    def __str__(self):
+        return self.name
+
+
+class Movies(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=300)
+    image = models.FileField(upload_to='movie_pic')
+    slug = AutoSlugField(populate_from = 'name')
+    video = models.FileField(upload_to='movie_video')
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    genre = models.ManyToManyField(Genre)
+
+    def __str__(self):
+        return self.name
